df031d0f6206479e71d08d5253379ec1c7ce500c
codedByBojan <jevdja990@gmail.com>
2025-06-16 09:54:34 +0200

    saving data in a txt file

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -16,15 +18,17 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         """ Update the displayed score on the game screen."""
-        self.write(f"Score: {self.score}", 
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", 
             align=ALIGNMENT, font=FONT)
         
-    def game_over(self):
-        """Ends the game and displays the outcome."""
-        self.goto(0, 0)
-        self.write(f"GAME OVER", 
-            align=ALIGNMENT, font=FONT)
-        
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()     
         
     def increase_score(self):
         """ Increase the current score and update the scoreboard."""
